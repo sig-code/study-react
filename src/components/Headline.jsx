7fa2aa3b8a1702c381b9d47ac7a83ecc8f0d8fed
sig-code <m.takku29@gmail.com>
2022-11-06 22:20:12 +0900

    🚚 srcディレクトリにファイルをまとめた

---
+++ src/components/Headline.jsx
@@ -0,0 +1,10 @@
+import classes from "./Headline.module.css";
+
+export function Headline({ page, children }) {
+  return (
+    <div>
+      <h1 className={classes.title}>{page} Page</h1>
+      <p className={classes.description}>Get started by editing {children}</p>
+    </div>
+  );
+}
